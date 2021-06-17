d925c8ed834acbbeb8c691b2cba9dcc0d94d4d10
sourav <souravdey.student@gmail.com>
2021-06-18 03:11:39 +0530

    login_system_done

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blogapp.urls')),
     path('',views.index),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index),
-    path('home/',views.home),
+    path('',views.index,name='index'),
+    path('home/',views.home,name='home'),
     path('login/',views.login,name='login'),
     path('signup/', views.signup,name='signup'),
     path('logout/', views.logout, name='logout'),
